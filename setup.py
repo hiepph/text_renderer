a013c8ea9b7f7c067b6d78b28e27c8600958fa38
hiepph <hiepph.2406@gmail.com>
2019-07-29 10:34:58 +0700

    feat: add setup.py script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+requirements = [
+    'opencv-python',
+    'Pillow',
+    'scipy',
+    'pygame',
+    'numpy',
+    'tqdm'
+]
+
+setup(
+    name='text_renderer',
+    version='0.0.1',
+    description="Generate Mjsynth text data",
+    packages=['text_renderer'],
+    install_requirements=requirements,
+    license='MIT license',
+)
